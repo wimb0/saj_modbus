6b8af2a0d76bf37dd69a5dbeadbad299bef3a0b6
wimb0 <w.baars@wimbo.nl>
2021-04-28 12:14:21 +0200

    add error reading

---
+++ read_invertor_errors.py
@@ -0,0 +1,159 @@
+import argparse
+import json
+
+from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.constants import Endian
+from pymodbus.exceptions import ConnectionException
+from pymodbus.payload import BinaryPayloadDecoder
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--host', help="SAJ Inverter IP",
+                    type=str, required=True)
+parser.add_argument('--port', help="SAJ Inverter Port",
+                    type=int, required=True)
+
+args = parser.parse_args()
+
+address = 0x0101  # First register with Inverter details.
+count = 6 # Read this amount of registers
+connected = False
+client = ModbusTcpClient(host=args.host, port=args.port, timeout=3)
+client.connect()
+
+FAULT_MESSAGES = {
+    0: {
+        0x80000000: "Code 81: Lost Communication D<->C",
+        0x00080000: "Code 48: Master Fan4 Error",
+        0x00040000: "Code 47: Master Fan3 Error",
+        0x00020000: "Code 46: Master Fan2 Error",
+        0x00010000: "Code 45: Master Fan1 Error",
+        0x00002000: "Code 43: Master HW Phase3 Current High",
+        0x00001000: "Code 42: Master HW Phase2 Current High",
+        0x00000800: "Code 41: Master HW Phase1 Current High",
+        0x00000400: "Code 40: Master HWPV2 Current High",
+        0x00000200: "Code 39: Master HWPV1 Current High",
+        0x00000100: "Code 38: Master HWBus Voltage High",
+        0x00000010: "Code 37: Master Phase3 Current High",
+        0x00000008: "Code 36: Master Phase2 Current High",
+        0x00000004: "Code 35: Master Phase1 Current High",
+        0x00000002: "Code 34: Master Bus Voltage Low",
+        0x00000001: "Code 33: Master Bus Voltage High",
+    },
+    1: {
+        0x80000000: "Code 32: Master Bus Voltage Balance Error",
+        0x40000000: "Code 31: Master ISO Error",
+        0x20000000: "Code 30: Master Phase3 DCI Error",
+        0x10000000: "Code 29: Master Phase2 DCI Error",
+        0x08000000: "Code 28: Master Phase1 DCI Error",
+        0x04000000: "Code 27: Master GFCI Error",
+        0x02000000: "Code 26: Master Phase3 No Grid Error",
+        0x01000000: "Code 25: Master Phase2 No Grid Error",
+        0x00800000: "Code 24: Master Phase1 No Grid Error",
+        0x00400000: "Code 23: Master Phase3 Frequency Low",
+        0x00200000: "Code 22: Master Phase3 Frequency High",
+        0x00100000: "Code 21: Master Phase2 Frequency Low",
+        0x00080000: "Code 20: Master Phase2 Frequency High",
+        0x00040000: "Code 19: Master Phase1 Frequency Low",
+        0x00020000: "Code 18: Master Phase1 Frequency High",
+        0x00010000: "Code 17: Master Phase3 Voltage 10Min High",
+        0x00008000: "Code 16: Master Phase2 Voltage 10Min High",
+        0x00004000: "Code 15: Master Phase1 Voltage 10Min High",
+        0x00002000: "Code 14: Master Phase3 Voltage Low",
+        0x00001000: "Code 13: Master Phase3 Voltage High",
+        0x00000800: "Code 12: Master Phase2 Voltage Low",
+        0x00000400: "Code 11: Master Phase2 Voltage High",
+        0x00000200: "Code 10: Master Phase1 Voltage Low",
+        0x00000100: "Code 09: Master Phase1 Voltage High",
+        0x00000080: "Code 08: Master Current Sensor Error",
+        0x00000040: "Code 07: Master DCI Device Error",
+        0x00000020: "Code 06: Master GFCI Device Error",
+        0x00000010: "Code 05: Master Lost Communication M<->S",
+        0x00000008: "Code 04: Master Temperature Low Error",
+        0x00000004: "Code 03: Master Temperature High Error",
+        0x00000002: "Code 02: Master EEPROM Error",
+        0x00000001: "Code 01: Master Relay Error",
+    },
+    2: {
+        0x40000000: "Code 80: Slave PV Voltage High Error",
+        0x20000000: "Code 79: Slave PV2 Current High Error",
+        0x10000000: "Code 78: Slave PV1 Current High Error",
+        0x08000000: "Code 77: Slave PV2 Voltage High Error",
+        0x04000000: "Code 76: Slave PV1 Voltage High Error",
+        0x02000000: "Code 75: Slave Phase3 No Grid Error",
+        0x01000000: "Code 74: Slave Phase2 No Grid Error",
+        0x00800000: "Code 73: Slave Phase1 No Grid Error",
+        0x00400000: "Code 72: Slave Phase3 Frequency Low",
+        0x00200000: "Code 71: Slave Phase3 Frequency High",
+        0x00100000: "Code 70: Slave Phase2 Frequency Low",
+        0x00080000: "Code 69: Slave Phase2 Frequency High",
+        0x00040000: "Code 68: Slave Phase1 Frequency Low",
+        0x00020000: "Code 67: Slave Phase1 Frequency High",
+        0x00010000: "Code 66: Slave Phase3 Voltage Low",
+        0x00008000: "Code 65: Slave Phase3 Voltage High",
+        0x00004000: "Code 64: Slave Phase2 Voltage Low",
+        0x00002000: "Code 63: Slave Phase2 Voltage High",
+        0x00001000: "Code 62: Slave Phase1 Voltage Low",
+        0x00000800: "Code 61: Slave Phase1 Voltage High",
+        0x00000400: "Code 60: Slave Phase3 DCI Consis Error",
+        0x00000200: "Code 59: Slave Phase2 DCI Consis Error",
+        0x00000100: "Code 58: Slave Phase1 DCI Consis Error",
+        0x00000080: "Code 57: Slave GFCI Consis Error",
+        0x00000040: "Code 56: Slave Phase3 Frequency Consis Error",
+        0x00000020: "Code 55: Slave Phase2 Frequency Consis Error",
+        0x00000010: "Code 54: Slave Phase1 Frequency Consis Error",
+        0x00000008: "Code 53: Slave Phase3 Voltage Consis Error",
+        0x00000004: "Code 52: Slave Phase2 Voltage Consis Error",
+        0x00000002: "Code 51: Slave Phase1 Voltage Consis Error",
+        0x00000001: "Code 50: Slave Lost Communication between M<->S",
+    },
+}
+
+
+try:
+    inverter_data = client.read_holding_registers(
+        unit=1, address=address, count=count)
+    connected = True
+except ConnectionException as ex:
+    print(f'Connecting to device {args.host} failed!')
+    connected = False
+
+if connected:
+    if not inverter_data.isError():
+        decoder = BinaryPayloadDecoder.fromRegisters(
+            inverter_data.registers, byteorder=Endian.Big)
+
+        faultMsg0 = decoder.decode_32bit_uint()
+        faultMsg1 = decoder.decode_32bit_uint()
+        faultMsg2 = decoder.decode_32bit_uint()
+
+        print("faultMsg "
+            + "{0:#010x}".format(faultMsg0)
+            + " "
+            + "{0:#010x}".format(faultMsg1)
+            + " "
+            + "{0:#010x}".format(faultMsg2)
+        )
+
+        faultMsg = []
+        if faultMsg0:
+            for code, mesg in FAULT_MESSAGES[0].items():
+                if faultMsg0 & code:
+                    faultMsg.append(mesg)
+        if faultMsg1:
+            for code, mesg in FAULT_MESSAGES[1].items():
+                if faultMsg1 & code:
+                    faultMsg.append(mesg)
+        if faultMsg2:
+            for code, mesg in FAULT_MESSAGES[2].items():
+                if faultMsg2 & code:
+                    faultMsg.append(mesg)
+
+        error = ", ".join(faultMsg)
+
+        if error:
+           print("Fault message: " + error)
+        else:
+           print("No faults")
+
+client.close()
