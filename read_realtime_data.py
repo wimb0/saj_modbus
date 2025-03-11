8388f17d68319a9fb25612b1b338e46d3f9b168a
Wim <11030068+wimb0@users.noreply.github.com>
2025-03-11 17:44:53 +0100

    Update read_realtime_data.py

--- read_realtime_data.py
+++ read_realtime_data.py
@@ -1,10 +1,7 @@
 import argparse
 import json
-
 from pymodbus.client import ModbusTcpClient
-from pymodbus.constants import Endian
 from pymodbus.exceptions import ConnectionException
-from pymodbus.payload import BinaryPayloadDecoder
 
 DEVICE_STATUSSES = {
     0: "Not Connected",
@@ -14,110 +11,81 @@ DEVICE_STATUSSES = {
     4: "Upgrading",
 }
 
-parser = argparse.ArgumentParser()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', help="SAJ Inverter IP", type=str, required=True)
+    parser.add_argument('--port', help="SAJ Inverter Port", type=int, required=True)
+    args = parser.parse_args()
 
-parser.add_argument('--host', help="SAJ Inverter IP",
-                    type=str, required=True)
-parser.add_argument('--port', help="SAJ Inverter Port",
-                    type=int, required=True)
+    address = 256  # First register with Realtime data.
+    count = 60  # Read this amount of registers
 
-args = parser.parse_args()
+    client = ModbusTcpClient(host=args.host, port=args.port, timeout=3)
+    client.connect()
 
-address = 256  # First register with Realtime data.
-count = 60  # Read this amount of registers
-connected = False
-client = ModbusTcpClient(host=args.host, port=args.port, timeout=3)
-client.connect()
+    try:
+        realtime_data = client.read_holding_registers(slave=1, address=address, count=count)
+        if realtime_data.isError():
+            raise ConnectionException("Error reading registers")
+    except ConnectionException as ex:
+        print(f'Connecting to device {args.host} failed: {ex}')
+        return
+    finally:
+        client.close()
 
-try:
-    realtime_data = client.read_holding_registers(
-        slave=1, address=address, count=count)
-    connected = True
-except ConnectionException as ex:
-    print(f'Connecting to device {args.host} failed!')
-    connected = False
+    # Gebruik de registers direct
+    registers = realtime_data.registers
 
-if connected:
-    if not realtime_data.isError():
-        decoder = BinaryPayloadDecoder.fromRegisters(
-            realtime_data.registers, byteorder=Endian.BIG
-        )
+    data = {
+            "mpvmode": registers[0],
+            "mpvstatus": DEVICE_STATUSSES.get(registers[0], "Unknown"),
+            "pv1volt": round(registers[6] * 0.1, 1),
+            "pv1curr": round(registers[7] * 0.01, 2),
+            "pv1power": round(registers[8] * 1, 0),
+            "pv2volt": round(registers[9] * 0.1, 1),
+            "pv2curr": round(registers[10] * 0.01, 2),
+            "pv2power": round(registers[11] * 1, 0),
+            "pv3volt": round(registers[12] * 0.1, 1),
+            "pv3curr": round(registers[13] * 0.01, 2),
+            "pv3power": round(registers[14] * 1, 0),
+            "busvolt": round(registers[15] * 0.1, 1),
+            "invtempc": round(registers[16] * 0.1, 1),
+            "gfci": registers[17],
+            "power": registers[18],
+            "qpower": registers[19],
+            "pf": round(registers[20] * 0.001, 3),
+            "l1volt": round(registers[21] * 0.1, 1),
+            "l1curr": round(registers[22] * 0.01, 2),
+            "l1freq": round(registers[23] * 0.01, 2),
+            "l1dci": registers[24],
+            "l1power": registers[25],
+            "l1pf": round(registers[26] * 0.001, 3),
+            "l2volt": round(registers[27] * 0.1, 1),
+            "l2curr": round(registers[28] * 0.01, 2),
+            "l2freq": round(registers[29] * 0.01, 2),
+            "l2dci": registers[30],
+            "l2power": registers[31],
+            "l2pf": round(registers[32] * 0.001, 3),
+            "l3volt": round(registers[33] * 0.1, 1),
+            "l3curr": round(registers[34] * 0.01, 2),
+            "l3freq": round(registers[35] * 0.01, 2),
+            "l3dci": registers[36],
+            "l3power": registers[37],
+            "l3pf": round(registers[38] * 0.001, 3),
+            "iso1": registers[39],
+            "iso2": registers[40],
+            "iso3": registers[41],
+            "iso4": registers[42],
+            "todayenergy": round(registers[43] * 0.01, 2),
+            "monthenergy": round((registers[44] << 16 | registers[45]) * 0.01, 2),
+            "yearenergy": round((registers[46] << 16 | registers[47]) * 0.01, 2),
+            "totalenergy": round((registers[48] << 16 | registers[49]) * 0.01, 2),
+            "todayhour": round(registers[50] * 0.1, 1),
+            "totalhour": round((registers[51] << 16 | registers[52]) * 0.1, 1),
+            "errorcount": registers[53]
+    }
+    json_data = json.dumps(data)
+    print(json_data)
 
-        data = {}
-
-        data["mpvmode"] = decoder.decode_16bit_uint()
-
-        if data["mpvmode"] in DEVICE_STATUSSES:
-            data["mpvstatus"] = DEVICE_STATUSSES[data["mpvmode"]]
-        else:
-            data["mpvstatus"] = "Unknown"
-
-        # TODO: read fault message
-        # faultmsg = decoder.decode_16bit_uint()
-        # skip 6 registers
-        decoder.skip_bytes(12)
-
-        data["pv1volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["pv1curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["pv1power"] = round(decoder.decode_16bit_uint() * 1, 0)
-
-        data["pv2volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["pv2curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["pv2power"] = round(decoder.decode_16bit_uint() * 1, 0)
-
-        data["pv3volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["pv3curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["pv3power"] = round(decoder.decode_16bit_uint() * 1, 0)
-
-        data["busvolt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-
-        data["invtempc"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-
-        data["gfci"] = decoder.decode_16bit_uint()
-
-        data["power"] = decoder.decode_16bit_uint()
-
-        data["qpower"] = decoder.decode_16bit_uint()
-
-        data["pf"] = round(decoder.decode_16bit_uint() * 0.001, 3)
-
-        data["l1volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["l1curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l1freq"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l1dci"] = decoder.decode_16bit_uint()
-        data["l1power"] = decoder.decode_16bit_uint()
-        data["l1pf"] = round(decoder.decode_16bit_uint() * 0.001, 3)
-
-        data["l2volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["l2curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l2freq"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l2dci"] = decoder.decode_16bit_uint()
-        data["l2power"] = decoder.decode_16bit_uint()
-        data["l2pf"] = round(decoder.decode_16bit_uint() * 0.001, 3)
-
-        data["l3volt"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["l3curr"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l3freq"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["l3dci"] = decoder.decode_16bit_uint()
-        data["l3power"] = decoder.decode_16bit_uint()
-        data["l3pf"] = round(decoder.decode_16bit_uint() * 0.001, 3)
-
-        data["iso1"] = decoder.decode_16bit_uint()
-        data["iso2"] = decoder.decode_16bit_uint()
-        data["iso3"] = decoder.decode_16bit_uint()
-        data["iso4"] = decoder.decode_16bit_uint()
-
-        data["todayenergy"] = round(decoder.decode_16bit_uint() * 0.01, 2)
-        data["monthenergy"] = round(decoder.decode_32bit_uint() * 0.01, 2)
-        data["yearenergy"] = round(decoder.decode_32bit_uint() * 0.01, 2)
-        data["totalenergy"] = round(decoder.decode_32bit_uint() * 0.01, 2)
-
-        data["todayhour"] = round(decoder.decode_16bit_uint() * 0.1, 1)
-        data["totalhour"] = round(decoder.decode_32bit_uint() * 0.1, 1)
-
-        data["errorcount"] = decoder.decode_16bit_uint()
-
-        json_data = json.dumps(data)
-        print(json_data)
-
-client.close()
+if __name__ == "__main__":
+    main()
