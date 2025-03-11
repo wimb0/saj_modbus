5abf6762e9530201a3eab2c6fc4bbda2de90909d
Wim <11030068+wimb0@users.noreply.github.com>
2025-03-11 18:09:51 +0100

    Update read_realtime_data.py

--- read_realtime_data.py
+++ read_realtime_data.py
@@ -67,11 +67,11 @@ def parse_registers(registers):
         "iso4": registers[43],
         "todayenergy": round(registers[44] * 0.01, 2),
         "monthenergy": round((registers[45] << 16 | registers[45]) * 0.01, 2),
-        "yearenergy": round((registers[46] << 16 | registers[47]) * 0.01, 2),
-        "totalenergy": round((registers[47] << 16 | registers[49]) * 0.01, 2),
-        "todayhour": round(registers[48] * 0.1, 1),
-        "totalhour": round((registers[49] << 16 | registers[52]) * 0.1, 1),
-        "errorcount": registers[50]
+        "yearenergy": round((registers[47] << 16 | registers[47]) * 0.01, 2),
+        "totalenergy": round((registers[49] << 16 | registers[49]) * 0.01, 2),
+        "todayhour": round(registers[51] * 0.1, 1),
+        "totalhour": round((registers[52] << 16 | registers[52]) * 0.1, 1),
+        "errorcount": registers[54]
     }
     return data
 
