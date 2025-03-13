5763d36161a4d4527e46c6200e487bdfea8f2052
Wim <11030068+wimb0@users.noreply.github.com>
2025-03-13 13:08:27 +0100

    Update read_inverter_details.py

--- read_inverter_details.py
+++ read_inverter_details.py
@@ -3,6 +3,7 @@ import json
 import logging
 from pymodbus.client import ModbusTcpClient
 from pymodbus.exceptions import ConnectionException
+from typing import List, Optional, Dict
 
 # Constants
 ADDRESS = 0x8F00  # First register with Inverter details
@@ -11,7 +12,7 @@ COUNT = 29  # Number of registers to read
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def read_inverter_data(client, address, count):
+def read_inverter_data(client: ModbusTcpClient, address: int, count: int) -> Optional[List[int]]:
     try:
         inverter_data = client.read_holding_registers(slave=1, address=address, count=count)
         if inverter_data.isError():
@@ -21,13 +22,16 @@ def read_inverter_data(client, address, count):
         logging.error(f'Connecting to device failed: {ex}')
         return None
 
-def parse_registers(registers):
+def parse_registers(registers: List[int]) -> Dict[str, str]:
+    def parse_string(registers_slice: List[int]) -> str:
+        return ''.join(chr(registers_slice[i] >> 8) + chr(registers_slice[i] & 0xFF) for i in range(len(registers_slice))).rstrip('\x00')
+
     return {
         "devicetype": str(registers[0]),
         "subtype": str(registers[1]),
         "commver": str(round(registers[2] * 0.001, 3)),
-        "serialnumber": ''.join(chr(registers[i] >> 8) + chr(registers[i] & 0xFF) for i in range(3, 13)).rstrip('\x00'),
-        "productcode": ''.join(chr(registers[i] >> 8) + chr(registers[i] & 0xFF) for i in range(13, 23)).rstrip('\x00'),
+        "serialnumber": parse_string(registers[3:13]),
+        "productcode": parse_string(registers[13:23]),
         "dispswver": str(round(registers[23] * 0.001, 3)),
         "masterctrlver": str(round(registers[24] * 0.001, 3)),
         "slavectrlver": str(round(registers[25] * 0.001, 3)),
@@ -36,14 +40,16 @@ def parse_registers(registers):
         "powerhwver": str(round(registers[28] * 0.001, 3))
     }
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', help="SAJ Inverter IP", type=str, required=True)
     parser.add_argument('--port', help="SAJ Inverter Port", type=int, required=True)
     args = parser.parse_args()
 
     client = ModbusTcpClient(host=args.host, port=args.port, timeout=3)
-    client.connect()
+    if not client.connect():
+        logging.error(f'Failed to connect to {args.host}:{args.port}')
+        return
 
     registers = read_inverter_data(client, ADDRESS, COUNT)
     client.close()
